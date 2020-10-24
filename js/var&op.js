ba08690d7d536dfe1074acbc3e281947dc9d7cce
Gr4L22 <umarinzalda@gmail.com>
2020-10-24 11:23:46 -0300

    first ex done

---
+++ js/var&op.js
@@ -0,0 +1,18 @@
+// EJ a
+var sum1, sum2;
+sum1=20;
+sum2=30;
+var sumatotal = sum1 + sum2;
+console.log(sumatotal);
+//Ej b
+var letra1 , letra2, letra3;
+letra1 ='Uriel';
+letra2 ='Marin';
+letra3 = letra1 + letra2;
+console.log(letra3);
+// Ej c
+var pal1 , pal2, letratotal;
+pal1 ='Uriel';
+pal2 ='Marin';
+letratotal = pal1.length + pal2.length;
+console.log(letratotal);
